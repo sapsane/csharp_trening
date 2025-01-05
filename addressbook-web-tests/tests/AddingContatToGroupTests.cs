502dbdf835a6acf16d585888f0513e683a6948d4
rotanov <rotanov.ev@gmail.com>
2025-01-05 17:09:10 +0300

    Задание №17:  добавления контактов в группы и удаления контактов из групп. Удаление готово. Добывление готово.

--- addressbook-web-tests/tests/AddingContatToGroupTests.cs
+++ addressbook-web-tests/tests/AddingContatToGroupTests.cs
@@ -13,6 +13,27 @@ namespace WebAddressbookTests
         [Test]
         public void TestAddingContactToGroup() 
         {
+
+            List<ContactData> contact4 = ContactData.GetAll();
+            if (contact4.Count == 0)
+            {
+                ContactData contact5 = new ContactData("TEST99", "Test99", "");
+                app.Contacts.CreateContact(contact5);
+            }
+
+
+            List<GroupData> group1 = GroupData.GetAll();
+            if (group1.Count == 0)
+            {
+                GroupData group2 = new GroupData("Test99");
+                group2.Header = "";
+                group2.Footer = "";
+
+                app.Groups.CreateGroup(group2);
+
+            }
+
+
             GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldList = group.GetContacts();
             ContactData contact = ContactData.GetAll().Except(oldList).First();
